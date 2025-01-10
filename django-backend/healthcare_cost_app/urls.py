453a365e7ecae358f2f91f59144e9074d933540d
Nicholas Araj <nicholasaraj@yahoo.com>
2025-01-09 21:19:32 -0800

    Health check API running successfully on Django backend

--- django-backend/healthcare_cost_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PricingRecordListView
-
-urlpatterns = [
-    path('pricing-records/', PricingRecordListView.as_view(), name='pricing-record-list'),
-]
