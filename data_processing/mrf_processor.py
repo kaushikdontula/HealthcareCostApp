1970b4f2151a2a64f7d6f09164645a4c3dcfa5c2
Gabriel Johnson <gabrielwjohnson1@gmail.com>
2025-01-24 14:11:59 -0500

    WIP

--- data_processing/mrf_processor.py
+++ data_processing/mrf_processor.py
@@ -7,10 +7,16 @@ from sqlalchemy import BigInteger
 from sqlalchemy.sql import insert
 from datetime import date
 
+import io
+
+import codecs
+# import collections
+
+from ijson import utils, common, compat
 
 from sqlalchemy.ext.compiler import compiles
 
-
+from ijson.common import IncompleteJSONError
 
 
 from sqlalchemy import create_engine, Column, Integer, Float, String, Date, Table, MetaData
@@ -41,20 +47,22 @@ from  models.MrfData import Provider
 from decimal import Decimal
  
 # file_path = "./mrf_files/2024-10-01_NEW_NW-COMMERCIAL-01_in-network-rates.json"
-file_path = "./mrf_files/2024-10-01_NEW_NW-COMMERCIAL-01_in-network-rates.json"
+file_path = "./mrf_files/2025-01-01_NEW_NW-COMMERCIAL-01_in-network-rates.json"
 # add try accept clause for errors
 # try utf8, then latin1, etc (check whatever byte code windows uses)
 # could also try to replace line endings with linux ones
 
-def main():
+# def main():
     # proccess MRF file
     # service_data = get_array_from_key(file_path, "in_network", 10)
     # # transform MRF data into 'Service' class objects
     # service_arr = service_data_to_objects(service_data)
     # insert Service class objects into DB in Service and Pricing table
 
-    provider_data = get_array_from_key(file_path, "provider_references", 2)
-    objects = provider_data_objects(provider_data)
+    # provider_data = get_array_from_key(file_path, "provider_references", 2)
+    # service_data = get_array_from_key(file_path, "in_network", 10000)
+
+    # objects = provider_data_objects(provider_data)
     # insert_into_database(service_arr)
 
 
@@ -92,16 +100,6 @@ def service_data_to_objects(data):
                 obj_arr.append(Mrf_obj)
     return obj_arr
 
-
-    # class DecimalEncoder(json.JSONEncoder):
-    #     def default(self, obj):
-    #         if isinstance(obj, Decimal):
-    #             return str(obj)
-    #         return super(DecimalEncoder, self).default(obj)
-    
-    # with open('output.json', 'w') as f:
-    #     json.dump(data, f, cls=DecimalEncoder, indent=2)
-
 def read_partial_json(file_path, limit=10):
     with open(file_path, 'rb') as file:
         parser = ijson.parse(file)
@@ -113,17 +111,24 @@ def read_partial_json(file_path, limit=10):
                 break
  
 def read_array_from_key(file_path, array_key, limit=10):
-    with open(file_path, 'rb') as file:
-        objects = ijson.items(file, f'{array_key}.item')
-        for i, obj in enumerate(objects):
-            if i >= limit:
-                break
-            print(obj)
- 
+    encodings = ['utf-8', 'latin1', 'utf-16', 'utf-8-sig', 'ascii']    
+    for enc in encodings:
+        try:
+            with open(file_path, 'rb', encoding=enc) as file:
+                
+                objects = ijson.items(file, f'{array_key}.item')
+        except (IncompleteJSONError, UnicodeDecodeError):
+            # objects = ijson.items(file, f'{array_key}.item')
+            continue
+
+    for i, obj in enumerate(objects):
+        if i >= limit:
+            break
+        print(obj)
 
 
-
-def get_array_from_key(file_path, array_key, limit=None):
+def get_array_from_key(file_path, array_key, limit=None):  
+    
     result = []
     with open(file_path, 'rb') as file:
         objects = ijson.items(file, f'{array_key}.item')
@@ -132,103 +137,6 @@ def get_array_from_key(file_path, array_key, limit=None):
                 break
             result.append(obj)
     return result
-
-
-
-
-# service_name - string - OF NOTE - I think this is maybe optionional but could help us during development
-# service_code - int
-# provider_references - array
-# negotiated_type - string
-# negotiated_rate - float
-# expiration_date - date
-# billing class - string
-
-def insert_into_database(mrf_obj_arr):
-
-
-    engine = create_engine('sqlite:///database/healthcare_pricing.db', echo=True)
-
-# Create the table if it doesn't exist
-
-    # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    metadata = MetaData()
-
-
     
-# Commit the transaction
-    
-    # with sqlite3.connect('database/healthcare_pricing.db') as conn:
-    for mrf_obj in mrf_obj_arr:
-        # service_id = add_service(session, mrf_obj, metadata)
-        pricing_id = add_pricing(session, mrf_obj, metadata)
-        # provider_service_id = add_provider_services(conn, mrf_obj)
 
-    print(f"inserted into db with id val {pricing_id}")
-
-# def add_provider_services(conn, service):
-#      sql = '''INSERT INTO ProviderService(negotiated_rate,negotiated_type,billing_class,expiration_date)
-#              VALUES(?,?,?,?) '''
-    
-#     cur = conn.cursor()
-
-#     # have to convert negotiaed rate to float or it bricks
-#     cur.execute(sql, [float(service.negotiated_rate), service.negotiated_type, service.billing_class,service.expiration_date])
-
-#     conn.commit()
-
-#     return cur.lastrowid
-
-
-def add_pricing(session, service, metadata):
-
-    pricing = Table(
-        "Pricing", metadata,  # Table name and metadata
-        Column("pricing_id", Integer, primary_key=True, autoincrement=True),  # Primary key with autoincrement
-        Column("negotiated_rate", Float, nullable=False),  # Non-nullable column
-        Column("negotiated_type", String, nullable=False),  # Non-nullable column
-        Column("billing_class", String, nullable=True),  # Nullable column
-        Column("expiration_date", Date, nullable=True)  # Nullable column
-    )
-    # insert table statement
-    result = session.execute(
-    insert(pricing)
-    .values(
-        negotiated_rate=service.negotiated_rate,
-        negotiated_type=service.negotiated_type,
-        billing_class=service.billing_class,
-        expiration_date=datetime.strptime(service.expiration_date, "%Y-%m-%d").date(),
-    )
-    .returning(pricing.c.pricing_id)  # Returning the primary key
-)
-
-    # have to convert negotiaed rate to float or it bricks
-    # cur.execute(sql, [service.negotiated_rate, service.negotiated_type, service.billing_class,service.expiration_date])
-
-    # session.commit()
-
-    return result
-
-def add_service(conn, service):
-    sql = '''INSERT INTO Services(billing_code,name,category)
-             VALUES(?,?,?) '''
-    
-    cur = conn.cursor()
-
-    cur.execute("SELECT 1 FROM Services WHERE billing_code = ?", (service.billing_code,))
-    exists = cur.fetchone() 
-
-    if(exists):
-        return -1
-
-    cur.execute(sql, [service.billing_code, service.service_name, None])
-
-    conn.commit()
-
-    return cur.lastrowid
-
-
-main()
+# main()
--- tests/test_mrf.py
+++ tests/test_mrf.py
@@ -3,12 +3,43 @@ import sys
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
-file_path = "./mrf_files/2024-10-01_NEW_NW-COMMERCIAL-01_in-network-rates.json"
+file_path = "./mrf_files/2025-01-01_NEW_NW-COMMERCIAL-01_in-network-rates.json"
 
 from data_processing import mrf_processor
 
 from  models.MrfData import Service
 from  models.MrfData import Provider
+
+# change into class, set up own info
+# magicmock
+
+# from unittest.mock import MagicMock
+ 
+# from FeeScheduleAutomation.src.db_access import DbAccess
+ 
+ 
+# class TestDbAccess(unittest.TestCase):
+#     def setUp(self):
+#         connection_string = "test_connection_string"
+#         self.db_access = DbAccess(connection_string)
+#         self.mock_engine = MagicMock()
+#         self.db_access.engine = self.mock_engine
+
+    # def test_sql_query_with_empty_query(self):
+    #         # Empty SQL query
+    #         sql_query = ""
+    #         test_params = ("test", "params")
+    
+    #         # Assertions
+    #         with self.assertRaises(ValueError):
+    #             self.db_access.sql_query_to_df(sql_query)
+    #         with self.assertRaises(ValueError):
+    #             self.db_access.execute_sql_query(sql_query)
+    #         with self.assertRaises(ValueError):
+    #             self.db_access.sql_query_to_df_with_params(sql_query, params=test_params)
+    #         with self.assertRaises(ValueError):
+    #             self.db_access.execute_sql_query_with_params(sql_query, params=test_params)
+
 
 def test_get_array_from_key_returns_array():
     res = mrf_processor.get_array_from_key(file_path, "provider_references", 2)
@@ -33,3 +64,22 @@ def test_service_data_objects_returns_class_instances():
     assert(res is not None)
     for obj in res:
         assert isinstance(obj, Service)
+
+def test_can_process_large_amounts_of_mrf_file():
+    service_data = mrf_processor.get_array_from_key(file_path, "in_network", 10000)
+    assert(service_data is not None)
+    assert isinstance(service_data, list)
+    service_arr = mrf_processor.service_data_to_objects(service_data)
+    assert(service_arr is not None)
+    for obj in service_arr:
+        assert isinstance(obj, Service)
+
+    provider_data = mrf_processor.get_array_from_key(file_path, "provider_references", 10000)
+    assert(provider_data is not None)
+    assert isinstance(provider_data, list)
+    assert (len(provider_data) == 10000)
+
+    provider_arr = mrf_processor.provider_data_objects(provider_data)
+    assert(provider_data is not None)
+    for obj in provider_arr:
+        assert isinstance(obj, Provider)
